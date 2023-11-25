2be0151881854935bcea0441de3d767458824af1
qinx3y <147278813+qinx3y@users.noreply.github.com>
2023-11-25 15:58:45 -0500

    formatted current time/wind speed/humidity/weather conditions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,43 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let conditionElement = document.querySelector("#weather-condition");
+  let date = new Date(response.data.time * 1000);
+
   cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  conditionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h `;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
